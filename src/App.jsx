302f6d4936e599abd4ad3f5901b830f0ff2ea714
rathoreanant98-eng <rathoreanant98@gmail.com>
2026-08-21 14:57:29 +0530

    feat: add mission section to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import AboutMission from "./components/AboutMission";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
+        <AboutMission />
       </main>
     </div>
   );
